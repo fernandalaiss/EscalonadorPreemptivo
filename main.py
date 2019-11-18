e0dbb311d68100e6d3ed32c7a7de166334bfd2db
Wellyson Gustavo Silva Oliveira <wellysongustavo@A205-02L.imd.local>
2019-11-18 16:55:24 -0300

    diga isso nao

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import os
 import time
+import signal
+
+def SIGINT_handler(sig_number, sig_stack):
+    	print ("So pegando sinal de ctrl+c mesmo")
 
 def round_robin():
-    print("")
-    lista_processos = ["./dummy1.py", "./dummy2.py"]
+    #tempo = int(input(" - Insira o tempo do dummy: "))
+    dummy1 = "./dummy1.py"
+    dummy2 = "./dummy2.py"
+    lista_processos = [dummy1, dummy2]
     lista_tempos = []
     contador = 0
+    aux = ""
     
     #falta ver como criar os processos e deixarem aguardando signal do escalonador
     #e pegar o tempo de entrada e em que o processo ficará em execução
-    for processo in lista_processos:
+    while lista_processos[0] != "":
         #print("O meu processo é ", processo, ".\n")
-        os.system(processo)
-        while(contador < quantum):
+        os.system(lista_processos[0])
+        while contador < quantum:
+            print(lista_processos[0])
+            time.sleep(1)
             contador += 1
         
-        if(contador == quantum):
+        if contador == quantum:
+            aux = lista_processos[0]
+            lista_processos[0] = lista_processos[1]
+            lista_processos[1] = aux
+            contador = 0
             ##envia SIGNAL para interromper processo e o coloca no fim da fila
-            print("já chega")    
+            #signal.signal(signal.SIGINT, SIGINT_handler)
+  
 
 def main():
     print ("Selecione o algoritmo de escalonamento\n (1) FIFO\n (2) Fair Share\n (3) Round Robin\n (0) Sair")
