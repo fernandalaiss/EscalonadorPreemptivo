295469f78490a177212c85eb97a82d7d8a9d4c8d
Fernanda Laís Pereira de Souza <fernandalaisouza@hotmail.com>
2019-11-20 05:17:55 -0300

    tentando criar proc e pegar seu pid

--- rr_teste.py
+++ rr_teste.py
@@ -6,34 +6,45 @@ import collections
 def SIGINT_handler(sig_number, sig_stack):
     	print ("So pegando sinal de ctrl+c mesmo")
 
-def round_robin():
+def getProcessos():
     tempo1 = int(input(" - Insira o tempo do dummy 1: "))
     tempo2 = int(input(" - Insira o tempo do dummy 2: "))
-    d1 = ("./dummy1.py", tempo1)
-    d2 = ("./dummy2.py", tempo2)
-    deque_processos = collections.deque([d1,d2])
-    
-    contador = 0
-    aux = ""
+    d1 = ("dummy1.py", tempo1)
+    d2 = ("dummy2.py", tempo2)
+    return collections.deque([d1,d2])
+
+def round_robin():
+
+    quantum = float(input(" - Insira o valor do quantum: "))
+    deque_processos = getProcessos()
     
     #falta ver como criar os processos e deixarem aguardando signal do escalonador
     #e pegar o tempo de entrada e em que o processo ficará em execução
     while deque_processos[0] != None:
-        
-        """os.system(dict_processos.get(key))
-        while contador < quantum:
-            print(lista_processos[0])
-            time.sleep(1)
-            contador += 1
-        
-        if contador == quantum:
-            aux = lista_processos[0]
-            lista_processos[0] = lista_processos[1]
-            lista_processos[1] = aux
-            contador = 0
-            ##envia SIGNAL para interromper processo e o coloca no fim da fila
-            #signal.signal(signal.SIGINT, SIGINT_handler)"""
-  
+        executavel = deque_processos[0][0]
+        tempo_do_executavel = deque_processos[0][1]
+        #comparar o tempo necessário para execução de cada processo com o tempo de quantum
+        #executa tudo se for menor que o quantum, executa o tempo de quantum e vai pro fim da fila com o que sobrar
+        pid_child = os.fork()
+        if pid_child == 0:
+            os.system("python3 "+executavel)
+        else:
+            if(tempo_do_executavel > quantum):
+                time.sleep(quantum)
+                deque_processos[0][1] -= quantum
+            
+            elif(tempo_do_executavel <= quantum):
+                time.sleep(tempo_do_executavel)
+                deque_processos[0][1] -= tempo_do_executavel
+            
+            os.kill(pid_child, signal.SIGINT)
+            
+            #usar rotate(-1) para mover primeiro elemento para ultima posicao
+            if(deque_processos[0][1] == 0):
+                deque_processos.popleft()
+            else:
+                deque_processos.rotate(-1)
+    
 
 def main():
     print ("Selecione o algoritmo de escalonamento\n (1) FIFO\n (2) Fair Share\n (3) Round Robin\n (0) Sair")
@@ -54,7 +65,6 @@ while escolha != "0":
         pass
     elif escolha == "3":
         print("\n########## ROUND ROBIN ###########")
-        quantum = float(input(" - Insira o valor do quantum: "))
         round_robin()
         pass
     else:
